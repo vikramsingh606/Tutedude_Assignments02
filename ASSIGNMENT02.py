9866a57d13f48479f7d42680f51e7fa6d973876e
vikramsingh606 <123637821+vikramsingh606@users.noreply.github.com>
2025-11-28 12:31:24 +0530

    Add files via upload

---
+++ ASSIGNMENT02.py
@@ -0,0 +1,30 @@
+"""
+Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+"""
+print("Task 1: - \n")
+num = int(input('Please Enter a number: '))
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number \n")
+
+print("Task 2: - \n")
+
+"""
+Task 2: Sum of Integers from 1 to 50 Using a Loop 
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range."""
+
+sumNumber = 0
+for i in range(1, 51):
+    sumNumber += i
+print(f"The sum of numbers 1 to 50 is : {sumNumber}")
+
+
+sumNumber = sum([i for i in range(1, 51)])
+print(f"in line:- The sum of numbers 1 to 50 is : {sumNumber}")
